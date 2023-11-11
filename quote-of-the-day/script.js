843ad396b2d87cdbe48b95006f5e65fbb15def36
xThat_IT_Guy <150445583+xRunsCode@users.noreply.github.com>
2023-11-12 03:37:52 +0600

    Add files via upload

---
+++ quote-of-the-day/script.js
@@ -0,0 +1,37 @@
+let api="https://api.quotable.io/quotes/random?tags=love|happiness";
+let quoteField=document.getElementById('quote-field');
+let author=document.getElementById('author-box');
+let quote="";
+
+// async function getQuote(url)
+// {
+//     let response= await fetch(url);
+//     let quote= await response.json();
+//     console.log(quote.author);
+// }
+
+
+async function getQuote(url) {
+    try {
+        let response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+         quote = await response.json();
+        // console.log(quote[0]['content']);
+        quoteField.innerHTML=quote[0]['content'];
+        author.innerHTML=quote[0]['author'];
+    } catch (e) {
+        console.log('Error:', e);
+    }
+}
+
+function tweet()
+{
+    let link="https://twitter.com/intent/tweet?text="+quote[0]['content'];
+    window.open(link);
+}
+
+
+
+getQuote(api);
